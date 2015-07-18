693f7ac2307a79f8f9736627f0ec52a61dbf3630
surreptakos <djgatsakos@gmail.com>
2015-07-17 20:56:22 -0500

    a

---
+++ App.java
@@ -0,0 +1,28 @@
+
+public class App {
+
+	public static void main(String[] args) {
+		/***** FAQs
+		 * Do I need a degree?
+		 * 	No, but it can help tremendously
+		 * 
+		 * Do I need experience?
+		 * 	No, but it's a big hurdle you need to get over
+		 * 	as a software developer
+		 * 	>	You need to know how to write software
+		 * 	>	Make some programs yourself
+		 * 	>	Even better if you can sell programs that 
+		 * 		you've made yourself
+		 * 	>	Websites for contracts/freelancing
+		 * 		~	guru.com
+		 * 	Make sure you've got a great CV/Resume
+		 * 
+		 * Strategy for finding work
+		 * 	Go to jobserve.com and search for jobs every day,
+		 * 	sort by latest
+		 * 
+		 * Stopped at 16:00
+		 */
+	}
+
+}
